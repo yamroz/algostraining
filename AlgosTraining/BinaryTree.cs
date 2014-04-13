935ed875aeae15f7f6fb8a8933b79cab560ba53e
YamroZ <yamroz@gmail.com>
2014-04-13 20:38:31 +0200

    This is initial commit, work on removing elements in progress.  Setting parent reference needs to be added in AddElement method.

---
+++ AlgosTraining/BinaryTree.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgosTraining
+{
+    public class TreeNode
+    {
+        public IComparable value = null;
+        public TreeNode parent = null;
+        public TreeNode left = null;
+        public TreeNode right = null;
+        public TreeNode(IComparable val)
+        {
+            value = val;
+        }
+    }
+
+    public class BinaryTree
+    {
+        private uint size;
+        private uint elToInsert;
+        private TreeNode root;
+        public BinaryTree()
+        {
+            size = 0;
+            elToInsert = 0;
+            this.root = null;
+        }
+
+        public void AddElement(IComparable el)
+        {
+            AddElement(el,ref root);
+            size++;
+        }
+
+        public void AddElements(ICollection<int> inputColl) //TODO check what the fuck is wrong with ICollection<IComparable>?!?!
+        {
+            foreach(IComparable el in inputColl)
+            {
+                AddElement(el);
+            }
+        }
+
+        private void AddElement(IComparable el, ref TreeNode tree)
+        {
+            if (tree != null)
+            {
+                if (el.CompareTo(tree.value) == 0) return; //already present in tree, we don't insert duplicates
+                if (el.CompareTo(tree.value) < 0)
+                {
+                    AddElement(el, ref tree.left);
+                }
+                else AddElement(el, ref tree.right);
+            }
+            else
+            {
+                tree = new TreeNode(el);
+            }
+        }
+        
+        private void GetTreeElements(TreeNode tree,ref IComparable[] result)
+        {
+            if (tree == null) return;
+            if (tree.left != null) GetTreeElements(tree.left, ref result);
+            result[elToInsert++] = tree.value;
+            if (tree.right != null) GetTreeElements(tree.right, ref result);
+        }
+
+        private TreeNode GetLargestElement(TreeNode tree,TreeNode parent = null)
+        {
+            if (parent != null) tree.parent = parent;   //TODO remove after adding parent to AddElement operation
+            else tree.parent = null;                    //TODO remove after adding parent to AddElement operation
+
+            if (tree.right == null)
+            {
+                return tree;
+            }
+            else return GetLargestElement(tree.right, tree);
+        }
+
+        public IComparable GetLargestElement()
+        {
+            return GetLargestElement(root).value;
+        }
+
+        private TreeNode GetSmallestElement(TreeNode tree, TreeNode parent = null)
+        {
+            if (parent != null) tree.parent = parent;   //TODO remove after adding parent to AddElement operation
+            else tree.parent = null;                    //TODO remove after adding parent to AddElement operation
+
+            if (tree.left == null) return tree;
+            else return GetSmallestElement(tree.left, tree);
+        }
+
+        public IComparable GetSmallestElement()
+        {
+            return GetSmallestElement(root).value;
+        }
+
+        /*
+         * After executing this method target is in place of source and has same children
+         */
+        private void ExchangeForSourceRemoval(TreeNode source, TreeNode target)
+        {
+            target.value = source.value;
+            if (source.parent.left == source) //source is left child
+            {
+                source.parent.left = target;
+            }
+            else //source is right child
+            {
+                source.parent.right = target;
+            }
+            target.left = source.left;
+            target.right = source.right;
+            target.parent = source.parent;
+        }
+
+        private void RemoveElement(TreeNode parent, TreeNode tree, IComparable el, bool weAreLeft)
+        {
+            //There is nothing to remove
+            if (tree == null) return;
+
+            //Element found:
+            if (tree.value.CompareTo(el) == 0)
+            {
+                //1. No descendants just remove.
+                if (tree.left == null && tree.right == null)
+                {
+                    //We are in root
+                    if (parent == null)
+                    {
+                        this.root = null; //deleting root
+                    }
+                    else // we are in regular node
+                    {
+                        if (weAreLeft) parent.left = null;
+                        else parent.right = null;
+                    }
+                }
+                else //2. there are descendants, we need to repair tree.
+                {
+                    TreeNode toRemove = tree;
+                    //lets choose candidate for substitute:
+                    if(tree.left != null)
+                    {
+                        TreeNode toSubstitute = GetLargestElement(tree.left);
+                        toSubstitute.parent.right = toSubstitute.left; //we are largest so parent right = our left
+                        ExchangeForSourceRemoval(toSubstitute, toRemove);
+
+                    }
+                    else 
+                    {
+                        TreeNode toSubstitute = GetSmallestElement(tree.right);
+                        toSubstitute.parent.left = toSubstitute.right; //we are smallest so parent left= our right
+                        ExchangeForSourceRemoval(toSubstitute, toRemove);
+                    }
+                }
+
+            }
+            //lets search further
+            else
+            {
+                //RemoveElement(tree, (el.CompareTo(tree.value)<0) ? tree.left : tree.right,el, true); //TODO try in the future
+                if (el.CompareTo(tree.value) < 0) RemoveElement(tree, tree.left, el, true); //we traverse left subtree
+                else RemoveElement(tree, tree.right, el, false);//we traverse right subtree
+            }
+        }
+
+        public IComparable[] GetElements()
+        {
+            IComparable[] result=new IComparable[size];
+            elToInsert = 0;
+            GetTreeElements(root, ref result);
+            return result;
+        }
+
+        public void RemoveElement(IComparable el)
+        {
+            RemoveElement(null, root, el, true);
+        }
+    }
+}
---
+++ AlgosTrainingUnitTests/BinaryTree.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgosTraining;
+
+namespace AlgosTrainingUnitTests
+{
+    [TestClass]
+    public class BinaryTreeUnitTests
+    {
+        [TestMethod]
+        public void CreateTree()
+        {
+            BinaryTree bt = new BinaryTree();
+        }
+
+        [TestMethod]
+        public void AddElementTest()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+        }
+
+
+        [TestMethod]
+        public void AddElementsTest()
+        {
+            int[] elementsToAdd = { 100, 150, 200, 300, 450, 5, 3, 7, 4, 2, 6, 7, 8, 4, 3, 100 };
+            BinaryTree bt = new BinaryTree();
+            bt.AddElements(elementsToAdd);
+            int[] expected = { 2, 3, 4, 5, 6, 7, 8, 100, 150, 200, 300, 450 };
+            IComparable[] result = bt.GetElements(); 
+            for (int i = 0; i < expected.Length; i++) Assert.AreEqual<IComparable>(expected[i], result[i], "The result and expected are different at index:" + i);
+        }
+
+        [TestMethod]
+        public void AddMultipleElementsTest()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+            bt.AddElement(1);
+            bt.AddElement(5);
+            bt.AddElement(4);
+            bt.AddElement(9);
+            bt.AddElement(3);
+            bt.AddElement(2);
+            bt.AddElement(7);
+        }
+
+        [TestMethod]
+        public void CheckSorting()
+        {
+            int[] expected = {1,2,3,4,5,7,9};
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+            bt.AddElement(1);
+            bt.AddElement(5);
+            bt.AddElement(4);
+            bt.AddElement(9);
+            bt.AddElement(3);
+            bt.AddElement(2);
+            bt.AddElement(7);
+            IComparable[] result = bt.GetElements();
+            for (int i = 0; i < expected.Length; i++) Assert.AreEqual<IComparable>(expected[i], result[i], "The result and expected are different at index:" + i);
+        }
+
+        [TestMethod]
+        public void AddNull()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(null);
+        }
+
+        [TestMethod]
+        public void RemoveElementFromProperTree()
+        {
+            int[] expected = { 1, 2, 4, 5, 7, 9 };
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+            bt.AddElement(1);
+            bt.AddElement(5);
+            bt.AddElement(4);
+            bt.AddElement(9);
+            bt.AddElement(3);
+            bt.AddElement(2);
+            bt.AddElement(7);
+            bt.RemoveElement(3);
+            IComparable[] result = bt.GetElements();
+            for (int i = 0; i < expected.Length; i++) Assert.AreEqual<IComparable>(expected[i], result[i], "The result and expected are different at index:" + i
+                + "\n result:" + result[0] + ',' + result[1] + ',' + result[2] + ',' + result[3] + ',' + result[4] + ',' + result[5] + ',' + result[6]);
+        }
+
+        [TestMethod]
+        public void GetSmallestElement()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+            bt.AddElement(1);
+            bt.AddElement(5);
+            bt.AddElement(4);
+            bt.AddElement(9);
+            bt.AddElement(3);
+            bt.AddElement(2);
+            bt.AddElement(7);
+            IComparable<int> expected = 1;
+            Assert.AreEqual(expected, bt.GetSmallestElement());
+        }
+
+        [TestMethod]
+        public void GetLargestElement()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.AddElement(5);
+            bt.AddElement(1);
+            bt.AddElement(5);
+            bt.AddElement(4);
+            bt.AddElement(9);
+            bt.AddElement(3);
+            bt.AddElement(2);
+            bt.AddElement(7);
+            IComparable<int> expected = 9;
+            Assert.AreEqual(expected, bt.GetLargestElement());
+        }
+
+        [TestMethod]
+        public void RemoveElementFromEmptyTree()
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.RemoveElement(5);
+        }
+    }
+}
